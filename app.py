2d0cac302aefa99aeefd1aeb16baeec5e13ed73b
Justin <jcoolip@gmail.com>
2026-07-18 21:55:20 -0400

    continue

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 # dummy app.py
+
+from flask import Flask, jsonify, redirect, render_template, request
+
+from modules.weather import get_weather
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", weather=get_weather())
+    # return render_template(
+    #   "index.html",
+    #   weather=get_weather(),
+    #   oldie=get_oldie_status(),
+    #   space=get_space_card(),
+    #   shopping=get_items(),
+    #   timers=get_active_timers()
+    # )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5020, debug=True)
---
+++ modules/weather.py
@@ -0,0 +1,8 @@
+def get_weather():
+    return {
+        "temp": 74,
+        "condition": "Partly Cloudy",
+        "high": 81,
+        "low": 61,
+        "location": "Beckley, WV",
+    }
